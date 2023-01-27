9258e78569e46d2e0a541db52c4d630d044c0de6
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-01-27 06:03:29 +0000

    listener impl. in activity & added as dependency to list fragment

--- 13_dual_pane_layouts/app/src/main/java/github/noargs/dualpanelayouts/ListFragment.kt
+++ 13_dual_pane_layouts/app/src/main/java/github/noargs/dualpanelayouts/ListFragment.kt
@@ -1,31 +1,25 @@
 package github.noargs.dualpanelayouts
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+class ListFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var starSignListener: StarSignListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is StarSignListener) {
+            starSignListener = context
+        } else {
+            throw RuntimeException("Must implement StarSignListener")
         }
     }
 
@@ -37,23 +31,33 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val starSigns = listOf<View>(
+            view.findViewById(R.id.aquarius),
+            view.findViewById(R.id.pisces),
+            view.findViewById(R.id.aries),
+            view.findViewById(R.id.taurus),
+            view.findViewById(R.id.gemini),
+            view.findViewById(R.id.cancer),
+            view.findViewById(R.id.leo),
+            view.findViewById(R.id.virgo),
+            view.findViewById(R.id.libra),
+            view.findViewById(R.id.scorpio),
+            view.findViewById(R.id.sagittarius),
+            view.findViewById(R.id.capricorn)
+        )
+
+        starSigns.forEach {
+            it.setOnClickListener(this)
+        }
+    }
+
+
+    override fun onClick(view: View?) {
+        view?.let { starSign ->
+            starSignListener.onSelected(starSign.id)
+        }
     }
 }
--- 13_dual_pane_layouts/app/src/main/java/github/noargs/dualpanelayouts/MainActivity.kt
+++ 13_dual_pane_layouts/app/src/main/java/github/noargs/dualpanelayouts/MainActivity.kt
@@ -3,9 +3,18 @@ package github.noargs.dualpanelayouts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+interface StarSignListener {
+    fun onSelected(id: Int)
+}
+
+class MainActivity : AppCompatActivity(), StarSignListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSelected(id: Int) {
+        TODO("Not yet implemented")
     }
 }
