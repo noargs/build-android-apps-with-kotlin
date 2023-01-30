d50dffd4961338d835d48042ae77b1431964de7b
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-01-30 04:52:19 +0000

    detail fragment wired for mobile and tablet

--- 13_dual_pane_layouts/app/src/main/java/github/noargs/dualpanelayouts/DetailFragment.kt
+++ 13_dual_pane_layouts/app/src/main/java/github/noargs/dualpanelayouts/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 
 
 class DetailFragment : Fragment() {
@@ -33,9 +34,69 @@ class DetailFragment : Fragment() {
         when (starSignId) {
             R.id.aquarius -> {
                 starSign?.text = getString(R.string.aquarius)
-                symbol?.text = getString(R.string.symbol, R.string.symbol_aquarius.toString())
-                dateRange?.text = getString(R.string.date_range, R.string.date_aquarius.toString() )
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_aquarius))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_aquarius) )
             }
+            R.id.pisces -> {
+                starSign?.text = getString(R.string.pisces)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_pisces))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_pisces))
+            }
+            R.id.aries -> {
+                starSign?.text = getString(R.string.aries)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_aries))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_aries))
+            }
+            R.id.taurus -> {
+                starSign?.text = getString(R.string.taurus)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_taurus))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_taurus))
+            }
+            R.id.gemini -> {
+                starSign?.text = getString(R.string.gemini)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_gemini))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_gemini))
+            }
+            R.id.cancer -> {
+                starSign?.text = getString(R.string.cancer)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_cancer))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_cancer))
+            }
+            R.id.leo -> {
+                starSign?.text = getString(R.string.leo)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_leo))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_leo))
+            }
+            R.id.virgo -> {
+                starSign?.text = getString(R.string.virgo)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_virgo))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_virgo))
+            }
+            R.id.libra -> {
+                starSign?.text = getString(R.string.libra)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_libra))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_libra))
+            }
+            R.id.scorpio -> {
+                starSign?.text = getString(R.string.scorpio)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_scorpio))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_scorpio))
+            }
+            R.id.sagittarius -> {
+                starSign?.text = getString(R.string.sagittarius)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_sagittarius))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_sagittarius))
+            }
+            R.id.capricorn -> {
+                starSign?.text = getString(R.string.capricorn)
+                symbol?.text = getString(R.string.symbol, getString(R.string.symbol_capricorn))
+                dateRange?.text = getString(R.string.date_range, getString(R.string.date_capricorn))
+            }
+            else -> {
+                Toast.makeText(context, getString(R.string.unknown_star_sign), Toast.LENGTH_LONG)
+                    .show();
+            }
+
         }
     }
 
--- 13_dual_pane_layouts/app/src/main/java/github/noargs/dualpanelayouts/MainActivity.kt
+++ 13_dual_pane_layouts/app/src/main/java/github/noargs/dualpanelayouts/MainActivity.kt
@@ -1,7 +1,11 @@
 package github.noargs.dualpanelayouts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+const val STAR_SIGN_ID = "STAR_SIGN_ID"
 
 interface StarSignListener {
     fun onSelected(id: Int)
@@ -9,12 +13,27 @@ interface StarSignListener {
 
 class MainActivity : AppCompatActivity(), StarSignListener {
 
+    var isDualPane: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        isDualPane = findViewById<View>(R.id.star_sign_detail) != null
     }
 
     override fun onSelected(id: Int) {
-        TODO("Not yet implemented")
+
+        if (isDualPane) {
+            val detailFragment = supportFragmentManager.findFragmentById(R.id.star_sign_detail) as DetailFragment
+            detailFragment.setStarSignData(id)
+        } else {
+            val detailIntent = Intent(
+                this,
+                DetailActivity::class.java).apply {
+                    putExtra(STAR_SIGN_ID, id)
+            }
+            startActivity(detailIntent)
+        }
     }
 }
