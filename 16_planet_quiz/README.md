18932cf4515265fcf61a089b84d9a30e44b66b9f
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-02-01 02:09:01 +0000

    init planet quizz app

---
+++ 16_planet_quiz/README.md
@@ -0,0 +1,7 @@
+### Planet quiz app   
+   
+    
+- Add `FragmentContainerView` into `app/build.gradle`
+```
+    implementation 'androidx.fragment:fragment-ktx:1.2.5'
+```
---
+++ 16_planet_quiz/app/src/main/java/github/noargs/planetquiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package github.noargs.planetquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
