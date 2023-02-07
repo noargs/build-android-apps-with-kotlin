1dd4003a07ce65efac7fe8395ce6292aa7071bca
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-02-07 23:52:58 +0000

    button_home ties to nav_home_to_content in onCreateView

--- 17_navigation_drawer/app/src/main/java/github/noargs/navigationdrawer/HomeFragment.kt
+++ 17_navigation_drawer/app/src/main/java/github/noargs/navigationdrawer/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,11 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        view.findViewById<Button>(R.id.button_home)?.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.nav_home_to_content, null)
+        )
+        return view
     }
 
     companion object {
