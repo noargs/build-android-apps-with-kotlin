c114e929667d44b7fa96451b97255dead0b6da93
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-02-08 05:32:44 +0000

    fragments wired to app_bar, mainActivity

--- 17_navigation_drawer/app/src/main/java/github/noargs/navigationdrawer/MainActivity.kt
+++ 17_navigation_drawer/app/src/main/java/github/noargs/navigationdrawer/MainActivity.kt
@@ -2,10 +2,41 @@ package github.noargs.navigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        // creating top level destinations and add them to the draw
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_recent, R.id.nav_favourites, R.id.nav_archive, R.id.nav_bin
+            ),
+            findViewById(R.id.drawer_layout)
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        findViewById<NavigationView>(R.id.nav_view)?.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
